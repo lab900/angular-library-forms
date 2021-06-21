de1b29a8c23d06abe43a5050947a4780e4e10d4e
lab900-senn-lambrecht <senn.lambrecht@lab900.com>
2021-06-21 14:47:31 +0200

    Fix import

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -10,9 +10,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
 
-import { Lab900FormsModule } from 'lib/src/lib/forms.module';
-import { Lab900NavListModule } from '@lab900/ui';
-import { Lab900PageHeaderModule } from '@lab900/ui';
+import { Lab900FormsModule } from '@lab900/forms';
+import { Lab900NavListModule, Lab900PageHeaderModule } from '@lab900/ui';
 
 import { ExampleViewerComponent } from './components/example-viewer/example-viewer.component';
 import { ShowcasePageComponent } from './components/showcase-page/showcase-page.component';
