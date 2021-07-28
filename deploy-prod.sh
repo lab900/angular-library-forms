5c5705477adcd6f97fa2a0091a59dd0bee2db13c
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2021-07-28 09:45:33 +0200

    Add SSH key for git access

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,6 +5,7 @@ cd ..
 npm i -g @angular/cli
 npm i
 npm run build:forms:prod
+ssh-add -K /workspace/git_ssh_key
 npm run deploy:showcase
 cd dist/@lab900/forms || exit
 npm config set -- '//registry.npmjs.org/:_authToken' "$1"
