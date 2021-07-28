bfa98cf359e6cbc85ea441f43e1262b9d9eb2a9d
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2021-07-28 10:19:49 +0200

    Fix permissions on ssh_key_file

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,11 +2,12 @@
 # Used in automatic deploy on main branch
 cd lib || exit
 cd ..
+eval "$(ssh-agent)" || exit
+chmod 400 /workspace/git_ssh_key || exit
+ssh-add /workspace/git_ssh_key || exit
 npm i -g @angular/cli
 npm i
 npm run build:forms:prod || exit
-eval "$(ssh-agent)" || exit
-ssh-add /workspace/git_ssh_key || exit
 npm run deploy:showcase || exit
 cd dist/@lab900/forms || exit
 npm config set -- '//registry.npmjs.org/:_authToken' "$1"
