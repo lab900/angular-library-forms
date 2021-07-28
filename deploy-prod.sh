8bdd67927126ed536a217fd82d73a421ab7a049b
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2021-07-28 10:00:36 +0200

    Fix ssh-add command

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,7 +6,7 @@ npm i -g @angular/cli
 npm i
 npm run build:forms:prod || exit
 eval "$(ssh-agent)" || exit
-ssh-add -K /workspace/git_ssh_key || exit
+ssh-add /workspace/git_ssh_key || exit
 npm run deploy:showcase || exit
 cd dist/@lab900/forms || exit
 npm config set -- '//registry.npmjs.org/:_authToken' "$1"
