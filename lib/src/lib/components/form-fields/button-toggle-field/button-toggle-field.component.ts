c2a1287625b272b0452be3ea0a1157739ab59ffe
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2022-04-19 15:04:15 +0200

    Deselect toggle button when clicking on selected button

--- lib/src/lib/components/form-fields/button-toggle-field/button-toggle-field.component.ts
+++ lib/src/lib/components/form-fields/button-toggle-field/button-toggle-field.component.ts
@@ -2,6 +2,7 @@ import { Component, HostBinding } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { FormComponent } from '../../AbstractFormComponent';
 import { FormFieldButtonToggle } from './button-toggle-field.model';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'lab900-button-toggle-field',
@@ -37,5 +38,19 @@ export class ButtonToggleFieldComponent extends FormComponent<FormFieldButtonTog
       (o) => o.value === this.value
     );
     return option?.label ?? `${this.value}`;
+  }
+
+  public onChange($event: MatButtonToggleChange): void {
+    if (this.options?.deselectOnClick && this.value === $event.value) {
+      setTimeout(() => {
+        this.group.controls[this.fieldAttribute].setValue(null);
+        this.group.controls[this.fieldAttribute].markAsDirty();
+        this.group.controls[this.fieldAttribute].markAsTouched();
+      });
+    } else {
+      setTimeout(() =>
+        this.group.controls[this.fieldAttribute].setValue($event.value)
+      );
+    }
   }
 }
