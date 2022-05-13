024baf7281648d0f47785fdd5d8305507f9cf204
Frederico Teixeira <frederico.teixeira@lab900.com>
2022-05-13 12:51:13 +0200

    feat: input suffix and prefix now acceps func

--- lib/src/lib/components/form-fields/input-field/input-field.component.ts
+++ lib/src/lib/components/form-fields/input-field/input-field.component.ts
@@ -22,6 +22,18 @@ export class InputFieldComponent extends FormComponent<FormFieldInput> {
       !!this.options?.showLengthIndicator
     );
   }
+  public get suffix(): string {
+    if (typeof this.options?.suffix === 'function') {
+      return this.options.suffix(this.group.value);
+    }
+    return this.options?.suffix;
+  }
+  public get prefix(): string {
+    if (typeof this.options?.prefix === 'function') {
+      return this.options.prefix(this.group.value);
+    }
+    return this.options?.prefix;
+  }
 
   public constructor(
     @Inject(LAB900_FORM_MODULE_SETTINGS)
