9e545cd7bf03af2eff9a7457c045bdbe74865466
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2022-08-31 10:37:36 +0200

    WIP: Automatic showcase deployments

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 # Used in automatic deploy on main branch
-cd lib || exit
+echo "$2" > git_ssh_key
+chmod 400 git_ssh_key || exit
+eval "$(ssh-agent)" || exit
+ssh-add git_ssh_key || exit
+mkdir ~/.ssh
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+cat ~/.ssh/config
+cat ~/.ssh/known_hosts
+mkdir test
+cd test || exit
+git clone git@github.com:lab900/angular-library-forms.git || exit
 cd ..
-npm i -g @angular/cli
-npm i
-npm run build:forms:prod
+npm i -g @angular/cli || exit
+npm i || exit
+npm run build:forms:prod || exit
+npm run deploy:showcase || exit
 cd dist/@lab900/forms || exit
 npm config set -- '//registry.npmjs.org/:_authToken' "$1"
 npm publish
