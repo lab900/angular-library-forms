adb509c280806cc4f88b1af6e475a2b65c3a1225
KoenDaelman <koen.daelman@lab900.com>
2022-09-02 14:02:37 +0200

    CI-CD: build

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Used in automatic deploy on main branch
+mkdir ./lib
+git clone git@github.com:lab900/angular-library-forms.git ./lib|| exit
+
 cd lib || exit
 cd ..
 npm i -g @angular/cli || exit
@@ -7,5 +10,5 @@ npm i || exit
 npm run build:forms:prod || exit
 npm run deploy:showcase || exit
 cd dist/@lab900/forms || exit
-npm config set -- '//registry.npmjs.org/:_authToken' "$1"
-npm publish
+#npm config set -- '//registry.npmjs.org/:_authToken' "$1"
+#npm publish
