bfe743916e3d10ee8de56791d53158aa30e2d125
KoenDaelman <koen.daelman@lab900.com>
2022-09-02 14:44:08 +0200

    CI-CD: build

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # Used in automatic deploy on main branch
 
-#echo "$2" > git_ssh_key
-#chmod 400 git_ssh_key || exit
-#eval "$(ssh-agent)" || exit
-#ssh-add git_ssh_key || exit
-#mkdir ~/.ssh
-#ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-#printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-#cat ~/.ssh/config
-#cat ~/.ssh/known_hosts
+echo "$2" > git_ssh_key
+chmod 400 git_ssh_key || exit
+eval "$(ssh-agent)" || exit
+ssh-add git_ssh_key || exit
+mkdir ~/.ssh
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+cat ~/.ssh/config
+cat ~/.ssh/known_hosts
 
 cd lib || exit
 cd ..
