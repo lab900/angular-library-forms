8ea997b83b50dd99351534a3b16bfc643a2fd25c
Gino Van Houdt <gino.vanhoudt@lab900.com>
2024-05-13 13:18:38 +0200

    fix/v16-slide-toggle-readonly

--- lib/src/lib/components/form-fields/slide-toggle-field/slide-toggle-field.component.ts
+++ lib/src/lib/components/form-fields/slide-toggle-field/slide-toggle-field.component.ts
@@ -5,6 +5,15 @@ import { FormFieldSlideToggle } from './slide-toggle-field.model';
 @Component({
   selector: 'lab900-slide-toggle-field',
   templateUrl: './slide-toggle-field.component.html',
+  styles: [
+    `
+      .readonly {
+        ::ng-deep * {
+          pointer-events: none !important;
+        }
+      }
+    `,
+  ],
 })
 export class SlideToggleFieldComponent extends FormComponent<FormFieldSlideToggle> {
   @HostBinding('class')
